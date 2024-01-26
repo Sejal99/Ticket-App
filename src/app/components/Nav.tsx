902be693a36b48ce20384e9b75516846c621d2a0
sejal99 <sejal8974@gmail.com>
2024-01-26 13:01:14 +0530

    next js setup

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <main>
+        <div>
+        <h1>this is navbar</h1>
+        </div>
+    </main>
+    
+  
+  )
+}
+
+export default Nav
