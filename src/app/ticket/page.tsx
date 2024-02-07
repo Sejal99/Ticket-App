6eac1dceea770c95730435567e96515ed73cc9ca
sejal99 <sejal8974@gmail.com>
2024-02-07 20:16:08 +0530

    new changes

--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -1,30 +1,30 @@
-// 'use client'
-// import React, { useState } from 'react';
+'use client'
+import React, { useState } from 'react';
 
 
-// const Page = () => {
+const Page = () => {
 
-//   const [title, setTitle] = useState('');
+  const [title, setTitle] = useState('');
 
-//   const handleChange = (e) => {
-//     setTitle(e.target.value);
-//   };
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
 
-//   return (
-//     <div className="flex flex-row p-3 m-2 bg-blue-500 w-45">
+  return (
+    <div className="flex flex-row p-3 m-2 bg-blue-500 w-45">
 
-//       <h1>Enter Title:</h1>
+      <h1>Enter Title:</h1>
   
-//       <input 
-//         type="text"
-//         value={title}
-//         onChange={handleChange}
-//         placeholder="Enter your title..."
-//         className='bg-blue p-5'
-//       />
+      <input 
+        type="text"
+        value={title}
+        onChange={handleChange}
+        placeholder="Enter your title..."
+        className='bg-blue p-5'
+      />
     
-//     </div>
-//   );
-// };
+    </div>
+  );
+};
 
-// export default Page;
+export default Page;
