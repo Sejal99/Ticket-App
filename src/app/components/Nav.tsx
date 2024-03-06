9c14a6f19704856b1e52272fab4f54d56302b4c6
sejal99 <sejal8974@gmail.com>
2024-03-06 21:34:58 +0530

    new changes

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,22 +1,22 @@
-// import Link from "next/link";
+import Link from "next/link";
 
 
-// const Nav = () => {
-//   return (
-//     <nav className="bg-white p-6">
-//       <div className="flex flex-row justify-between">
-//         <div className="flex flex-row gap-4">
-//           <h1>Home</h1>
+const Nav = () => {
+  return (
+    <nav className="bg-white p-6">
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row gap-4">
+          <h1>Home</h1>
       
-//           <Link href="/ticket">Create Ticket</Link>
+          <Link href="/ticket">Create Ticket</Link>
           
-//         </div>
-//         <div>
-//           <h1>sejal@gmail.com</h1>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
+        </div>
+        <div>
+          <h1>sejal@gmail.com</h1>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-// export default Nav;
+export default Nav;
